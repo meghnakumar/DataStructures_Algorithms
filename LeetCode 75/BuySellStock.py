9c315a604fb7ae7e998a1a9526709307c1366b9a
Meghna Kumar <meghna1120@gmail.com>
2022-05-12 22:47:31 -0300

    Best Time to Buy and Sell Stock You are given an array prices where prices[i] is the price of a given stock on the ith day. You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.

--- LeetCode 75/BuySellStock.py
+++ LeetCode 75/BuySellStock.py
@@ -1,6 +1,7 @@
 #Leetcode question Link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
 
 class Solution(object):
+    #Time: O(n2)
     def maxProfit_bruteForce(self, prices):
         """
         :type prices: List[int]
@@ -12,4 +13,22 @@ class Solution(object):
                 if prices[i] < prices[j]:
                     profit = max(prices[j] - prices[i], profit)
 
+        return profit
+
+#Optimized solution
+    #Time = O(n)
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        profit = 0
+        left = 0
+        right = 1
+        while right < len(prices):
+            if prices[right] > prices[left]:
+                profit = max(prices[right] - prices[left], profit)
+            else:
+                left = right
+            right = right + 1;
         return profit

b9cbda4da02d9496c77b5ef044d1ab5a07f31772
Meghna Kumar <31566039+meghnakumar@users.noreply.github.com>
2022-08-08 17:03:50 -0300

    Time: 34 ms (21.15%), Space: 13.5 MB (64.22%) - LeetHub

--- 100-same-tree/100-same-tree.py
+++ 100-same-tree/100-same-tree.py
@@ -13,14 +13,6 @@ class Solution(object):
         """
         if p is not None and q is not None:
             if p.val == q.val:
-                # if p.left is None or q.left is None:
-                #     return False
-                # if p.right is None or q.right is None:
-                #     return False
-                # if p.left is not None and q.left is not None:
-                #     return self.isSameTree(p.left,q.left)
-                # if p.right is not None and q.right is not None:
-                #     return self.isSameTree(p.right,q.right)
                 return self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right)
         else:
             return p==q

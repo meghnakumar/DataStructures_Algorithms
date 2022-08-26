9cd2cd96e63a8187785e8a157ac9df73e4fd1648
Meghna Kumar <31566039+meghnakumar@users.noreply.github.com>
2022-08-25 23:41:44 -0300

    Time: 36 ms (11.64%), Space: 13.4 MB (36.20%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -1,15 +1,18 @@
 class Solution(object):
-    def climbStairs(self, n):
+    def climbStairs(self, n, stairs=0,memo={}):
         """
         :type n: int
         :rtype: int
         """
-        first =1
-        second=1
-        for i in range(n-1):
-            temp=first
-            first=second
-            second=temp+second
-        return second
+        if n==0:
+            return 1
+        elif n<0:
+            return 0
+        if n-1 not in memo:
+            memo[n-1] =  self.climbStairs(n-1,memo)
+        if n-2 not in memo:
+            memo[n-2] = self.climbStairs(n-2,memo)
+        return memo[n-1]+memo[n-2]
+        
         
         
